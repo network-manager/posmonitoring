ebb286724390f9703bf4d13c96d1104e43c68eea
dtekeshe <david@tekeshe.com>
2019-04-21 07:35:38 +0200

    ádd pos board and grafana docker

---
+++ grafana/rungrafana.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#We use the grafana image that Grafana Labs provides http://docs.grafana.org/installation/docker/
+# If you wish to modify the port that Grafana runs on, you can do that here.
+sudo docker run -d -p 3000:3000 --net="host" --hostname grafana --restart=always --name grafana grafana/grafana
+
